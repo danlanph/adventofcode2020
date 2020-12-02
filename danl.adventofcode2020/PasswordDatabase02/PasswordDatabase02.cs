f8265bbedca7dcd6282d14e3c4e2025fc54815bf
Dan Lanphier <dan.lanphier@nationwide.co.uk>
2020-12-02 20:43:42 +0000

    feat: added both parts of day 2 puzzle

---
+++ danl.adventofcode2020/PasswordDatabase02/PasswordDatabase02.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace danl.adventofcode2020.PasswordDatabase02
+{
+    [Puzzle(puzzleNumber: 2, numberOfParts: 1)]
+    public class PasswordDatabase02
+    {
+        public const string InputFileResourceName = "danl.adventofcode2020.PasswordDatabase02.input.txt";
+
+        public readonly PasswordDatabaseEntry[] PasswordDatabase;
+
+        public PasswordDatabase02(string inputString)
+        {
+            PasswordDatabase = inputString
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(row =>
+                {
+                    var rowFields = row.Split(':', 2);
+                    var passwordPolicy = rowFields[0].Trim();
+                    var password = rowFields[1].Trim();
+
+                    return new PasswordDatabaseEntry
+                    {
+                        PasswordPolicyString = passwordPolicy,
+                        Password = password
+                    };
+                })
+                .ToArray();
+        }
+
+        public static void Run(int part)
+        {
+            var passwordDatabase = new PasswordDatabase02(InputHelper.GetResourceFileAsString(InputFileResourceName));
+            var passwordPolicyValidator = default(IPasswordPolicyValidator);
+
+            switch (part)
+            {
+                case 1:
+                    passwordPolicyValidator = new SledRentalPasswordPolicyValidator();
+                    break;
+                case 2:
+                    passwordPolicyValidator = new TobogganRentalPasswordPolicyValidator();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            var numberOfValidPasswords = passwordDatabase.GetInvalidPasswordCount(passwordPolicyValidator);
+            Console.WriteLine($"There are {numberOfValidPasswords} valid passwords.");
+        }
+
+        public int GetInvalidPasswordCount(IPasswordPolicyValidator passwordPolicyValidator)
+        {
+            return PasswordDatabase.Count(x => IsPasswordEntryValid(x, passwordPolicyValidator));
+        }
+
+        public static bool IsPasswordEntryValid(PasswordDatabaseEntry passwordDatabaseEntry, IPasswordPolicyValidator passwordPolicyValidator)
+        {
+            passwordPolicyValidator.LoadPolicy(passwordDatabaseEntry.PasswordPolicyString);
+            return passwordPolicyValidator.ValidatePassword(passwordDatabaseEntry.Password);
+        }
+    }
+}
---
+++ danl.adventofcode2020/PasswordDatabase02/PasswordDatabaseEntry.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace danl.adventofcode2020.PasswordDatabase02
+{
+    public class PasswordDatabaseEntry
+    {
+        public string PasswordPolicyString { get; set; }
+        public string Password { get; set; }
+    }
+}
