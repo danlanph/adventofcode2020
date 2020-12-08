5150b810936034e8ec9860f8130b97ab67a0aff1
Daniel Lanphier <daniel.lanphier@adp.com>
2020-12-08 22:37:47 +0000

    feat: added both parts of day 7 puzzle

---
+++ danl.adventofcode2020/LuggageControl07/Bag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace danl.adventofcode2020.LuggageControl07
+{
+    public class Bag
+    {        
+        public string Color { get; private set; }
+
+        public IList<BagQuantity> ContainedBags { get; private set; }
+
+        public Bag(string color)
+        {
+            Color = color;
+            ContainedBags = new List<BagQuantity>();
+        }
+    }
+
+    public class BagQuantity
+    {
+        public int Quantity { get; set; }
+
+        public Bag Bag { get; set; }
+    }
+}
